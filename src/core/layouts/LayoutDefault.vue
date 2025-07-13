6df8db18bd28a14bcd237f0d6009f1f4efdcbb84
DevJ <josegperezdev@gmail.com>
2025-07-13 01:27:41 -0600

    terrain images inside scoped style test

--- src/core/layouts/LayoutDefault.vue
+++ src/core/layouts/LayoutDefault.vue
@@ -2,11 +2,12 @@
 import Navbar from '@/core/components/Navbar.vue'
 import Footer from '@/core/components/Footer.vue'
 
-const backgroundImage = "background: url(/terrain/macroland250.png)"
+// const backgroundImage = "background: url(/terrain/macroland250.png)"
+const backgroundImage = "";
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col" :style=backgroundImage>
+  <div class="min-h-screen flex flex-col auto-scroll-bg" :style=backgroundImage>
     <Navbar />
     <main class="flex-grow">
       <slot />
@@ -14,3 +15,21 @@ const backgroundImage = "background: url(/terrain/macroland250.png)"
     <Footer />
   </div>
 </template>
+
+<style scoped>
+@keyframes scrollBackground {
+  0% {
+    background-position: 0 0;
+  }
+
+  100% {
+    background-position: 100% 100%;
+  }
+}
+
+.auto-scroll-bg {
+  background-image: url('/terrain/macroland250.png');
+  background-repeat: repeat;
+  animation: scrollBackground 30s linear infinite;
+}
+</style>
